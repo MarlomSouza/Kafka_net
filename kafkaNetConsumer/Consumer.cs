9b8e1480873c4869570f93e2e628b698472b657f
Marlom Souza <marlomsouza@digix.com.br>
2018-09-24 18:12:05 -0400

    Mensagem especifica

--- kafkaNetConsumer/Consumer.cs
+++ kafkaNetConsumer/Consumer.cs
@@ -13,12 +13,13 @@ namespace kafkaNetConsumer
     {
         static Dictionary<string, object> conf;
         private static string topic = "IDGTestTopic";
+        private static Consumer<Ignore, string> GetConsumer() => new Consumer<Ignore, string>(conf, null, new StringDeserializer(Encoding.UTF8));
 
         static void Main(string[] args)
         {
             conf = new Dictionary<string, object>
             {
-            { "group.id", Guid.NewGuid() },
+            { "group.id", "Grupo" },
             { "bootstrap.servers", "localhost:9092" },
             { "enable.auto.commit", false},
             {"enable.auto.offset.store", false},
@@ -30,13 +31,12 @@ namespace kafkaNetConsumer
             };
             // ConsumirFila();
             //consumir mensagem especifica
-            ConsumirMensagemEspecifica();
-            
+            ConsumirMensagemEspecifica(10);
         }
 
         private static void ConsumirFila()
         {
-            using (var consumer = new Consumer<Null, string>(conf, null, new StringDeserializer(Encoding.UTF8)))
+            using (var consumer = GetConsumer())
             {
                 consumer.OnMessage += (_, msg) =>
                 {
@@ -51,70 +51,48 @@ namespace kafkaNetConsumer
                 consumer.OnConsumeError += (_, msg)
                   => Console.WriteLine($"Consume error ({msg.TopicPartitionOffset}): {msg.Error}");
 
-                
                 while (true)
-                {
                     BuscarNovaMensagem(consumer);
-                }
             }
         }
 
-        private static void BuscarNovaMensagem(Consumer<Null, string> consumer) =>
+        private static void BuscarNovaMensagem(Consumer<Ignore, string> consumer) =>
             consumer.Poll(TimeSpan.FromMilliseconds(10));
-        
 
-        private static void ImprimirMensagem(Message<Null, string> msg) =>
-            Console.WriteLine($"Read '{msg.Value}' from: {msg.TopicPartitionOffset}");
+        private static void ImprimirMensagem(Message<Ignore, string> msg) =>
+            Console.WriteLine($"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
 
-     public static void ConsumirMensagemEspecifica()
+        public static void ConsumirMensagemEspecifica(long posicalInicial)
         {
-            conf = new Dictionary<string, object>{
-            { "group.id", new Guid().ToString() },
-                { "bootstrap.servers", "localhost:9092" },
-                // partition offsets can be committed to a group even by consumers not
-                // subscribed to the group. in this example, auto commit is disabled
-                // to prevent this from occuring.
-                { "enable.auto.commit", false }
-            };
-            using (var consumer = new Consumer<Ignore, string>(conf, null, new StringDeserializer(Encoding.UTF8)))
+            using (var consumer = GetConsumer())
             {
-                consumer.Assign(ConsumirDeOnde());
+                consumer.Assign(ConsumirAPartirDaPosicao(posicalInicial));
 
-                // Raised on critical errors, e.g. connection failures or all brokers down.
+                consumer.OnMessage += (_, msg) =>
+                {
+                    var err = consumer.CommitAsync().Result.Error;
+                    if (!err)
+                        Console.WriteLine($"OUTRO TO: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+                };
+
                 consumer.OnError += (_, error)
                     => Console.WriteLine($"Error: {error}");
 
-                // Raised on deserialization errors or when a consumed message has an error != NoError.
                 consumer.OnConsumeError += (_, error)
                     => Console.WriteLine($"Consume error: {error}");
 
                 while (true)
                 {
                     if (consumer.Consume(out Message<Ignore, string> msg, TimeSpan.FromSeconds(1)))
-                    {
-                        Console.WriteLine($"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
-                    }
+                        ImprimirMensagem(msg);
                 }
             }
         }
 
-        private static IEnumerable<TopicPartitionOffset> ConsumirDeOnde()
+        private static IEnumerable<TopicPartitionOffset> ConsumirAPartirDaPosicao(long posicalInicial)
         {
-            // TopicPartitionOffset topico = ParticaoOffSet();
-            var topicos = new[] { new TopicPartitionOffset(topic, 0, Offset.Beginning) };
-            return topicos;
-        }
-
-        private static TopicPartitionOffset ParticaoOffSet()
-        {
-            var topico = new TopicPartitionOffset(topic, 0, Offset.Beginning);
-            return topico;
-        }
-
-        private static TopicPartition[] CriarTopicos()
-        {
-            TopicPartition partition0 = new TopicPartition(topic, 0);
-            var topicos = new[] { partition0 };
+            var off = new Offset(posicalInicial);
+            var topicos = new[] { new TopicPartitionOffset(topic, 0, off) };
             return topicos;
         }
     }
